3f4daaed9479eae660add45c8e7b41eb753f7335
Vaibhav20201 <vaibhav_k@mt.iitr.ac.in>
2021-11-27 20:44:46 +0530

    added proxy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
+import '@openzeppelin/hardhat-upgrades';
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
