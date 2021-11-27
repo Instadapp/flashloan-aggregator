6f8637bda0e819b31105e20907a2ac2b6cb53ef1
Vaibhav Khanna <55931792+Vaibhav20201@users.noreply.github.com>
2021-11-27 20:52:03 +0530

    Updated Proxy name
    Co-authored-by: Samyak Jain <34437877+KaymasJain@users.noreply.github.com>

--- contracts/proxy/instapool.sol
+++ contracts/proxy/instapool.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract InstaPoolV2 is TransparentUpgradeableProxy {
+contract InstaFlashAggregator is TransparentUpgradeableProxy {
     constructor(address _logic, address admin_, bytes memory _data) public TransparentUpgradeableProxy(_logic, admin_, _data) {}
 }
