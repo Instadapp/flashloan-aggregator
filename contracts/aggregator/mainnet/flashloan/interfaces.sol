9ac682f5dd5cdee1603b11b6110d77598bf7cb6f
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2021-11-29 00:28:11 +0530

    transfer fee update

--- contracts/aggregator/mainnet/flashloan/interfaces.sol
+++ contracts/aggregator/mainnet/flashloan/interfaces.sol
@@ -23,26 +23,10 @@ interface ListInterface {
     function accountID(address) external view returns (uint64);
 }
 
-interface TokenInterface {
-    function approve(address, uint256) external;
-
-    function transfer(address, uint256) external;
-
-    function transferFrom(
-        address,
-        address,
-        uint256
-    ) external;
-
-    function deposit() external payable;
-
-    function withdraw(uint256) external;
-
-    function balanceOf(address) external view returns (uint256);
+interface TokenInterface is IERC20 {
 
     function decimals() external view returns (uint256);
 
-    function allowance(address, address) external view returns (uint256);
 }
 
 interface CTokenInterface {
