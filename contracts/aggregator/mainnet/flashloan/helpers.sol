4147f513dfc828a9d5601641a1f3dc438ad0d4b2
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2021-12-23 16:59:53 +0530

    fixed approve issue

--- contracts/aggregator/mainnet/flashloan/helpers.sol
+++ contracts/aggregator/mainnet/flashloan/helpers.sol
@@ -22,19 +22,11 @@ contract Helper is Variables {
      * @param amount_ amount of token.
     */
     function approve(IERC20 token_, address spender_, uint256 amount_) internal {
-        try token_.approve(spender_, amount_) {
-
-        } catch {
-            try token_.approve(spender_, 0) {
-  
-            } catch {
-                token_.safeApprove(spender_, 0);
-            }
-            try token_.approve(spender_, amount_) {
-
-            } catch {
-                token_.safeApprove(spender_, amount_);
-            }           
+        if(token_.allowance(address(this), spender_) == 0) {
+            token_.safeApprove(spender_, amount_);
+        } else {
+            token_.safeApprove(spender_, 0);
+            token_.safeApprove(spender_, amount_);
         }
     }
 
