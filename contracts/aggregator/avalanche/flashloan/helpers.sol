f25196a7e1feecbe55ec7920d462eb3a2755d531
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-02-17 16:24:31 +0530

    prettified avalanche

--- contracts/aggregator/avalanche/flashloan/helpers.sol
+++ contracts/aggregator/avalanche/flashloan/helpers.sol
@@ -6,10 +6,7 @@ import {Variables} from "./variables.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import { 
-    TokenInterface,
-    InstaFlashReceiverInterface
-} from "./interfaces.sol";
+import {TokenInterface, InstaFlashReceiverInterface} from "./interfaces.sol";
 
 contract Helper is Variables {
     using SafeERC20 for IERC20;
@@ -20,12 +17,14 @@ contract Helper is Variables {
      * @param token_ token for which allowance is to be given.
      * @param spender_ the address to which the allowance is to be given.
      * @param amount_ amount of token.
-    */
-    function approve(address token_, address spender_, uint256 amount_) internal {
+     */
+    function approve(
+        address token_,
+        address spender_,
+        uint256 amount_
+    ) internal {
         TokenInterface tokenContract_ = TokenInterface(token_);
-        try tokenContract_.approve(spender_, amount_) {
-            
-        } catch {
+        try tokenContract_.approve(spender_, amount_) {} catch {
             IERC20 token = IERC20(token_);
             token.safeApprove(spender_, 0);
             token.safeApprove(spender_, amount_);
@@ -38,17 +37,24 @@ contract Helper is Variables {
      * @param _instaLoanVariables struct which includes list of token addresses and amounts.
      * @param _fees list of premiums/fees for the corresponding addresses for flashloan.
      * @param _receiver address to which tokens have to be approved.
-    */
+     */
     function safeApprove(
         FlashloanVariables memory _instaLoanVariables,
         uint256[] memory _fees,
         address _receiver
     ) internal {
         uint256 length_ = _instaLoanVariables._tokens.length;
-        require(length_ == _instaLoanVariables._amounts.length, "Lengths of parameters not same");
+        require(
+            length_ == _instaLoanVariables._amounts.length,
+            "Lengths of parameters not same"
+        );
         require(length_ == _fees.length, "Lengths of parameters not same");
-        for (uint i = 0; i < length_; i++) {
-            approve(_instaLoanVariables._tokens[i], _receiver, _instaLoanVariables._amounts[i] + _fees[i]);
+        for (uint256 i = 0; i < length_; i++) {
+            approve(
+                _instaLoanVariables._tokens[i],
+                _receiver,
+                _instaLoanVariables._amounts[i] + _fees[i]
+            );
         }
     }
 
@@ -57,14 +63,17 @@ contract Helper is Variables {
      * @notice Transfers the tokens to the receiver address.
      * @param _instaLoanVariables struct which includes list of token addresses and amounts.
      * @param _receiver address to which tokens have to be transferred.
-    */
+     */
     function safeTransfer(
         FlashloanVariables memory _instaLoanVariables,
         address _receiver
     ) internal {
         uint256 length_ = _instaLoanVariables._tokens.length;
-        require(length_ == _instaLoanVariables._amounts.length, "Lengths of parameters not same");
-        for (uint i = 0; i < length_; i++) {
+        require(
+            length_ == _instaLoanVariables._amounts.length,
+            "Lengths of parameters not same"
+        );
+        for (uint256 i = 0; i < length_; i++) {
             IERC20 token = IERC20(_instaLoanVariables._tokens[i]);
             token.safeTransfer(_receiver, _instaLoanVariables._amounts[i]);
         }
@@ -75,14 +84,15 @@ contract Helper is Variables {
      * @notice Calculates the balances of the account passed for the tokens.
      * @param _tokens list of token addresses to calculate balance for.
      * @param _account account to calculate balance for.
-    */
-    function calculateBalances(
-        address[] memory _tokens,
-        address _account
-    ) internal view returns (uint256[] memory) {
+     */
+    function calculateBalances(address[] memory _tokens, address _account)
+        internal
+        view
+        returns (uint256[] memory)
+    {
         uint256 _length = _tokens.length;
         uint256[] memory balances_ = new uint256[](_length);
-        for (uint i = 0; i < _length; i++) {
+        for (uint256 i = 0; i < _length; i++) {
             IERC20 token = IERC20(_tokens[i]);
             balances_[i] = token.balanceOf(_account);
         }
@@ -93,12 +103,18 @@ contract Helper is Variables {
      * @dev Validates if the receiver sent the correct amounts of funds.
      * @notice Validates if the receiver sent the correct amounts of funds.
      * @param _instaLoanVariables struct which includes list of initial balances, final balances and fees for the respective tokens.
-    */
-    function validateFlashloan(
-        FlashloanVariables memory _instaLoanVariables
-    ) internal pure {
-        for (uint i = 0; i < _instaLoanVariables._iniBals.length; i++) {
-            require(_instaLoanVariables._iniBals[i] + _instaLoanVariables._instaFees[i] <= _instaLoanVariables._finBals[i], "amount-paid-less");
+     */
+    function validateFlashloan(FlashloanVariables memory _instaLoanVariables)
+        internal
+        pure
+    {
+        for (uint256 i = 0; i < _instaLoanVariables._iniBals.length; i++) {
+            require(
+                _instaLoanVariables._iniBals[i] +
+                    _instaLoanVariables._instaFees[i] <=
+                    _instaLoanVariables._finBals[i],
+                "amount-paid-less"
+            );
         }
     }
 
@@ -106,10 +122,10 @@ contract Helper is Variables {
      * @dev Validates if token addresses are unique. Just need to check adjacent tokens as the array was sorted first
      * @notice Validates if token addresses are unique.
      * @param _tokens list of token addresses.
-    */
+     */
     function validateTokens(address[] memory _tokens) internal pure {
-        for (uint i = 0; i < _tokens.length - 1; i++) {
-            require(_tokens[i] != _tokens[i+1], "non-unique-tokens");
+        for (uint256 i = 0; i < _tokens.length - 1; i++) {
+            require(_tokens[i] != _tokens[i + 1], "non-unique-tokens");
         }
     }
 
@@ -117,14 +133,18 @@ contract Helper is Variables {
      * @dev Returns fee for the passed route in BPS.
      * @notice Returns fee for the passed route in BPS. 1 BPS == 0.01%.
      * @param _route route number for flashloan.
-    */
-    function calculateFeeBPS(uint256 _route) public view returns(uint256 BPS_){
+     */
+    function calculateFeeBPS(uint256 _route)
+        public
+        view
+        returns (uint256 BPS_)
+    {
         if (_route == 1) {
             BPS_ = aaveLending.FLASHLOAN_PREMIUM_TOTAL();
         } else {
             revert("Invalid source");
         }
-        
+
         if (BPS_ < InstaFeeBPS) {
             BPS_ = InstaFeeBPS;
         }
@@ -135,12 +155,16 @@ contract Helper is Variables {
      * @notice Calculate fees for the respective amounts and fee in BPS passed. 1 BPS == 0.01%.
      * @param _amounts list of amounts.
      * @param _BPS fee in BPS.
-    */
-    function calculateFees(uint256[] memory _amounts, uint256 _BPS) internal pure returns (uint256[] memory) {
+     */
+    function calculateFees(uint256[] memory _amounts, uint256 _BPS)
+        internal
+        pure
+        returns (uint256[] memory)
+    {
         uint256 length_ = _amounts.length;
         uint256[] memory InstaFees = new uint256[](length_);
-        for (uint i = 0; i < length_; i++) {
-            InstaFees[i] = (_amounts[i] * _BPS) / (10 ** 4);
+        for (uint256 i = 0; i < length_; i++) {
+            InstaFees[i] = (_amounts[i] * _BPS) / (10**4);
         }
         return InstaFees;
     }
@@ -150,23 +174,37 @@ contract Helper is Variables {
      * @notice Sort the tokens and amounts arrays according to token addresses.
      * @param _tokens list of token addresses.
      * @param _amounts list of respective amounts.
-    */
-    function bubbleSort(address[] memory _tokens, uint256[] memory _amounts) internal pure returns (address[] memory, uint256[] memory) {
+     */
+    function bubbleSort(address[] memory _tokens, uint256[] memory _amounts)
+        internal
+        pure
+        returns (address[] memory, uint256[] memory)
+    {
         for (uint256 i = 0; i < _tokens.length - 1; i++) {
-            for( uint256 j = 0; j < _tokens.length - i - 1 ; j++) {
-                if(_tokens[j] > _tokens[j+1]) {
-                    (_tokens[j], _tokens[j+1], _amounts[j], _amounts[j+1]) = (_tokens[j+1], _tokens[j], _amounts[j+1], _amounts[j]);
+            for (uint256 j = 0; j < _tokens.length - i - 1; j++) {
+                if (_tokens[j] > _tokens[j + 1]) {
+                    (
+                        _tokens[j],
+                        _tokens[j + 1],
+                        _amounts[j],
+                        _amounts[j + 1]
+                    ) = (
+                        _tokens[j + 1],
+                        _tokens[j],
+                        _amounts[j + 1],
+                        _amounts[j]
+                    );
                 }
             }
         }
         return (_tokens, _amounts);
-    }  
+    }
 
     /**
      * @dev Returns to true if the passed address is a DSA else returns false.
      * @notice Returns to true if the passed address is a DSA else returns false.
      * @param _account account to check for, if DSA.
-    */
+     */
     function checkIfDsa(address _account) internal view returns (bool) {
         return instaList.accountID(_account) > 0;
     }
@@ -175,10 +213,13 @@ contract Helper is Variables {
      * @dev  better checking by double encoding the data.
      * @notice better checking by double encoding the data.
      * @param data_ data passed.
-    */
+     */
     modifier verifyDataHash(bytes memory data_) {
         bytes32 dataHash_ = keccak256(data_);
-        require(dataHash_ == dataHash && dataHash_ != bytes32(0), "invalid-data-hash");
+        require(
+            dataHash_ == dataHash && dataHash_ != bytes32(0),
+            "invalid-data-hash"
+        );
         require(status == 2, "already-entered");
         dataHash = bytes32(0);
         _;
@@ -188,8 +229,8 @@ contract Helper is Variables {
     /**
      * @dev reentrancy gaurd.
      * @notice reentrancy gaurd.
-    */
-    modifier reentrancy {
+     */
+    modifier reentrancy() {
         require(status == 1, "already-entered");
         status = 2;
         _;
--- contracts/aggregator/avalanche/flashloan/variables.sol
+++ contracts/aggregator/avalanche/flashloan/variables.sol
@@ -1,30 +1,26 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import { 
-    IndexInterface,
-    ListInterface,
-    IAaveLending
-} from "./interfaces.sol";
+import {IndexInterface, ListInterface, IAaveLending} from "./interfaces.sol";
 
 contract ConstantVariables {
-
-    address public constant aaveLendingAddr = 0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C;
+    address public constant aaveLendingAddr =
+        0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C;
     IAaveLending public constant aaveLending = IAaveLending(aaveLendingAddr);
 
-    address public constant treasuryAddr = 0xE06d0b1752E60687C0EA5ABBe006d3368fdCDCC1;
-    address private constant instaListAddr = 0x9926955e0Dd681Dc303370C52f4Ad0a4dd061687;
+    address public constant treasuryAddr =
+        0xE06d0b1752E60687C0EA5ABBe006d3368fdCDCC1;
+    address private constant instaListAddr =
+        0x9926955e0Dd681Dc303370C52f4Ad0a4dd061687;
     ListInterface public constant instaList = ListInterface(instaListAddr);
 
     uint256 public constant InstaFeeBPS = 5; // in BPS; 1 BPS = 0.01%
-
 }
 
 contract Variables is ConstantVariables {
-
     bytes32 internal dataHash;
     // if 1 then can enter flashlaon, if 2 then callback
-    uint internal status;
+    uint256 internal status;
 
     struct FlashloanVariables {
         address[] _tokens;
@@ -33,5 +29,4 @@ contract Variables is ConstantVariables {
         uint256[] _finBals;
         uint256[] _instaFees;
     }
-
 }
