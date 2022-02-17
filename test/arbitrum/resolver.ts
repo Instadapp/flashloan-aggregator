e2ba7f256cd285c10ca68ea16e1c372812d9ca5d
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-02-17 16:45:30 +0530

    prettified tests

--- test/arbitrum/resolver.ts
+++ test/arbitrum/resolver.ts
@@ -1,40 +1,41 @@
-const hre = require("hardhat");
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-const { ethers } = hre;
+const hre = require('hardhat')
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+const { ethers } = hre
 
 import {
   InstaFlashloanResolverArbitrum,
   InstaFlashloanResolverArbitrum__factory,
-} from "../../typechain";
+} from '../../typechain'
 
-describe("Resolver", function () {
-  let Resolver, resolver: InstaFlashloanResolverArbitrum;
-  let signer: SignerWithAddress;
+describe('Resolver', function () {
+  let Resolver, resolver: InstaFlashloanResolverArbitrum
+  let signer: SignerWithAddress
 
-  const USDC = "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8";
-  const USDT = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
+  const USDC = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'
+  const USDT = '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9'
 
-  const Usdc = ethers.utils.parseUnits("5000", 6);
-  const Usdt = ethers.utils.parseUnits("5000", 6);
+  const Usdc = ethers.utils.parseUnits('5000', 6)
+  const Usdt = ethers.utils.parseUnits('5000', 6)
 
   beforeEach(async function () {
-    [signer] = await ethers.getSigners();
+    ;[signer] = await ethers.getSigners()
 
-    Resolver = new InstaFlashloanResolverArbitrum__factory(signer);
-    resolver = await Resolver.deploy();
+    Resolver = new InstaFlashloanResolverArbitrum__factory(signer)
+    resolver = await Resolver.deploy()
     await resolver.deployed()
-  });
+  })
 
-  it("Should be able to return routes info", async function () {
-    console.log((await resolver.getRoutesInfo()).toString());
-  });
+  it('Should be able to return routes info', async function () {
+    console.log((await resolver.getRoutesInfo()).toString())
+  })
 
-  it("Should be able to return the best route for flashloan", async function () {
-    console.log((await resolver.getBestRoutes([USDC, USDT], [Usdc, Usdt])).toString());
-  });
+  it('Should be able to return the best route for flashloan', async function () {
+    console.log(
+      (await resolver.getBestRoutes([USDC, USDT], [Usdc, Usdt])).toString(),
+    )
+  })
 
-  it("Should be able to return all the data for flashloan", async function () {
-    console.log((await resolver.getData([USDC, USDT], [Usdc, Usdt])).toString());
-  });
-  
-});
+  it('Should be able to return all the data for flashloan', async function () {
+    console.log((await resolver.getData([USDC, USDT], [Usdc, Usdt])).toString())
+  })
+})
--- test/mainnet/resolver.ts
+++ test/mainnet/resolver.ts
@@ -1,41 +1,42 @@
-const hre = require("hardhat");
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-const { ethers } = hre;
+const hre = require('hardhat')
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+const { ethers } = hre
 
 import {
   InstaFlashloanResolver,
-  InstaFlashloanResolver__factory
-} from "../../typechain";
+  InstaFlashloanResolver__factory,
+} from '../../typechain'
 
-describe("Resolver", function () {
-  let Resolver, resolver: InstaFlashloanResolver;
-  let signer: SignerWithAddress;
+describe('Resolver', function () {
+  let Resolver, resolver: InstaFlashloanResolver
+  let signer: SignerWithAddress
 
-  const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f";
-  const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+  const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f'
+  const USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7'
 
-  const Dai = ethers.utils.parseUnits("5000", 18);
-  const Usdt = ethers.utils.parseUnits("5000", 6);
+  const Dai = ethers.utils.parseUnits('5000', 18)
+  const Usdt = ethers.utils.parseUnits('5000', 6)
 
   beforeEach(async function () {
-    [signer] = await ethers.getSigners();
+    ;[signer] = await ethers.getSigners()
 
-    Resolver = new InstaFlashloanResolver__factory(signer);
-    resolver = await Resolver.deploy();
+    Resolver = new InstaFlashloanResolver__factory(signer)
+    resolver = await Resolver.deploy()
     await resolver.deployed()
-  });
+  })
 
-  it("Should be able to return routes info", async function () {
-    console.log((await resolver.getRoutesInfo()).toString());
-  });
+  it('Should be able to return routes info', async function () {
+    console.log((await resolver.getRoutesInfo()).toString())
+  })
 
-  it("Should be able to return the best route for flashloan", async function () {
-    console.log((await resolver.getBestRoutes([DAI, USDT], [Dai, Usdt])).toString());
-    console.log((await resolver.getBestRoutes([DAI], [Dai])).toString());
-  });
+  it('Should be able to return the best route for flashloan', async function () {
+    console.log(
+      (await resolver.getBestRoutes([DAI, USDT], [Dai, Usdt])).toString(),
+    )
+    console.log((await resolver.getBestRoutes([DAI], [Dai])).toString())
+  })
 
-  it("Should be able to return all the data for flashloan", async function () {
-    console.log((await resolver.getData([DAI, USDT], [Dai, Usdt])).toString());
-  });
-  
-});
+  it('Should be able to return all the data for flashloan', async function () {
+    console.log((await resolver.getData([DAI, USDT], [Dai, Usdt])).toString())
+  })
+})
