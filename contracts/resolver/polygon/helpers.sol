b07576564c88a05ee162e3e545dc76ea9c6aefb9
PIYUSH ANAND <anandpiyush2000@gmail.com>
2022-02-25 17:22:28 +0530

    Resolver changes for polygon

--- contracts/resolver/polygon/helpers.sol
+++ contracts/resolver/polygon/helpers.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Variables} from "./variables.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IUniswapV3Pool} from "./interfaces.sol";
 
 contract Helper is Variables {
     function getAaveAvailability(
@@ -52,6 +53,9 @@ contract Helper is Variables {
                     routesWithAvailability_[j] = _routes[i];
                     j++;
                 }
+            } else if (_routes[i] == 8) {
+                routesWithAvailability_[j] = _routes[i];
+                j++;
             } else {
                 require(false, "invalid-route");
             }
@@ -87,6 +91,146 @@ contract Helper is Variables {
     function validateTokens(address[] memory _tokens) internal pure {
         for (uint256 i = 0; i < _tokens.length - 1; i++) {
             require(_tokens[i] != _tokens[i + 1], "non-unique-tokens");
+        }
+    }
+
+    
+
+     function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
+        require(key.token0 < key.token1,"Token not sorted");
+        pool = address(
+            uint160(uint256(
+                keccak256(
+                    abi.encodePacked(
+                        hex'ff',
+                        factory,
+                        keccak256(abi.encode(key.token0, key.token1, key.fee)),
+                        POOL_INIT_CODE_HASH
+                    )
+                )
+            ))
+        );
+    }
+
+
+
+    function getUniswapBestFee(
+        address[] memory _tokens,
+        uint256[] memory _amounts
+    ) internal  returns (PoolKey memory) {
+        uint256 length = _tokens.length;
+        if (length == 1) {
+            address token0 = _tokens[0];
+
+            PoolKey memory bestKey;
+            bestKey.fee = type(uint24).max;
+
+            PoolKey memory key1;
+            PoolKey memory key2;
+            PoolKey memory key3;
+
+            if (token0 != Weth) {
+                if (token0 < Weth) {
+                    key1.token0 = token0;
+                    key1.token1 = Weth;
+                } else {
+                    key1.token0 = Weth;
+                    key1.token1 = token0;
+                }
+                key1.fee = 100;
+            }
+
+            if (token0 != USDC) {
+                if (token0 < USDC) {
+                    key2.token0 = token0;
+                    key2.token1 = USDC;
+                } else {
+                    key2.token0 = USDC;
+                    key2.token1 = token0;
+                }
+                key2.fee = 500;
+            }
+
+            if (token0 != MATIC) {
+                if (token0 < MATIC) {
+                    key3.token0 = token0;
+                    key3.token1 = MATIC;
+                } else {
+                    key3.token0 = MATIC;
+                    key3.token1 = token0;
+                }
+                key3.fee = 3000;
+            }
+
+            address uniswapPoolAddress1 = computeAddress(factory, key1);
+            IUniswapV3Pool pool1 = IUniswapV3Pool(uniswapPoolAddress1);
+
+            if (pool1.balance0() >= _amounts[0]) {
+                if (key1.fee < bestKey.fee) bestKey = key1;
+            }
+
+            address uniswapPoolAddress2 = computeAddress(factory, key2);
+            IUniswapV3Pool pool2 = IUniswapV3Pool(uniswapPoolAddress2);
+
+            if (pool2.balance0() >= _amounts[0]) {
+                if (key2.fee < bestKey.fee) bestKey = key2;
+            }
+
+            address uniswapPoolAddress3 = computeAddress(factory, key3);
+            IUniswapV3Pool pool3 = IUniswapV3Pool(uniswapPoolAddress3);
+
+            if (pool3.balance0() >= _amounts[0]) {
+                if (key3.fee < bestKey.fee) bestKey = key3;
+            }
+
+            return bestKey;
+        } else {
+            bubbleSort(_tokens, _amounts);
+            address token0 = _tokens[0];
+            address token1 = _tokens[1];
+
+            PoolKey memory bestKey;
+            bestKey.fee = type(uint24).max;
+
+            PoolKey memory key;
+
+            key.token0 = token0;
+            key.token1 = token1;
+            key.fee = 100;
+
+            address uniswapPoolAddress1 = computeAddress(factory, key);
+            IUniswapV3Pool pool1 = IUniswapV3Pool(uniswapPoolAddress1);
+
+            if (
+                pool1.balance0() >= _amounts[0] &&
+                pool1.balance1() >= _amounts[1]
+            ) {
+                if (key.fee < bestKey.fee) bestKey = key;
+            }
+
+            key.fee = 500;
+            address uniswapPoolAddress2 = computeAddress(factory, key);
+            IUniswapV3Pool pool2 = IUniswapV3Pool(uniswapPoolAddress2);
+
+            if (
+                pool2.balance0() >= _amounts[0] &&
+                pool2.balance1() >= _amounts[1]
+            ) {
+                if (key.fee < bestKey.fee) bestKey = key;
+            }
+
+            key.fee = 3000;
+            address uniswapPoolAddress3 = computeAddress(factory, key);
+            IUniswapV3Pool pool3 = IUniswapV3Pool(uniswapPoolAddress3);
+
+            if (
+                pool3.balance0() >= _amounts[0] &&
+                pool3.balance1() >= _amounts[1]
+            ) {
+                if (key.fee < bestKey.fee) bestKey = key;
+            }
+
+            return bestKey;
         }
     }
 }
--- contracts/resolver/polygon/main.sol
+++ contracts/resolver/polygon/main.sol
@@ -21,13 +21,18 @@ contract FlashResolverPolygon is Helper {
 
     function getBestRoutes(address[] memory _tokens, uint256[] memory _amounts)
         public
-        view
-        returns (uint16[] memory, uint256)
+        returns (
+            uint16[] memory,
+            uint256,
+            bytes[] memory
+        )
     {
         require(_tokens.length == _amounts.length, "array-lengths-not-same");
 
         (_tokens, _amounts) = bubbleSort(_tokens, _amounts);
         validateTokens(_tokens);
+
+        bytes[] memory _data;
 
         uint16[] memory bRoutes_;
         uint256 feeBPS_;
@@ -39,42 +44,64 @@ contract FlashResolverPolygon is Helper {
         );
         uint16 j = 0;
         bRoutes_ = new uint16[](routes_.length);
+        _data = new bytes[](routes_.length);
         feeBPS_ = type(uint256).max;
         for (uint256 i = 0; i < routesWithAvailability_.length; i++) {
-            if (routesWithAvailability_[i] != 0) {
+            if (routesWithAvailability_[i] == 8) {
+                uint256 length = _tokens.length;
+                require(length == 1 || length == 2, "Number of tokens exceed");
+                PoolKey memory bestKey = getUniswapBestFee(_tokens, _amounts);
+                bytes memory dt;
+                dt = abi.encode(bestKey);
+                if (feeBPS_ > bestKey.fee) {
+                    feeBPS_ = uint256(bestKey.fee);
+                    bRoutes_[0] = routesWithAvailability_[i];
+                    _data[0] = dt;
+                    j = 1;
+                } else if (feeBPS_ == bestKey.fee) {
+                    bRoutes_[j] = routesWithAvailability_[i];
+                    _data[j] = dt;
+                    j++;
+                }
+            } else if (routesWithAvailability_[i] != 0) {
                 uint256 routeFeeBPS_ = flashloanAggregator.calculateFeeBPS(
                     routesWithAvailability_[i]
                 );
+                bytes memory dt;
                 if (feeBPS_ > routeFeeBPS_) {
                     feeBPS_ = routeFeeBPS_;
                     bRoutes_[0] = routesWithAvailability_[i];
+                    _data[0] = dt;
                     j = 1;
                 } else if (feeBPS_ == routeFeeBPS_) {
                     bRoutes_[j] = routesWithAvailability_[i];
+                    _data[j] = dt;
                     j++;
                 }
             }
         }
         uint16[] memory bestRoutes_ = new uint16[](j);
+        bytes[] memory bestData_ = new bytes[](j);
         for (uint256 i = 0; i < j; i++) {
             bestRoutes_[i] = bRoutes_[i];
+            bestData_[i] = _data[i];
         }
-        return (bestRoutes_, feeBPS_);
+        return (bestRoutes_, feeBPS_, bestData_);
     }
 
     function getData(address[] memory _tokens, uint256[] memory _amounts)
         public
-        view
         returns (
             uint16[] memory routes_,
             uint256[] memory fees_,
             uint16[] memory bestRoutes_,
-            uint256 bestFee_
+            uint256 bestFee_,
+            bytes[] memory bestData_
         )
     {
         (routes_, fees_) = getRoutesInfo();
-        (bestRoutes_, bestFee_) = getBestRoutes(_tokens, _amounts);
-        return (routes_, fees_, bestRoutes_, bestFee_);
+        (bestRoutes_, bestFee_,bestData_) = getBestRoutes(_tokens, _amounts);
+        return (routes_, fees_, bestRoutes_, bestFee_,bestData_);
     }
 }
 
