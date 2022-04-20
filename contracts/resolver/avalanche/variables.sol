933b3e04a2a510882bd9d63b7ecf5b51cf907a08
Shriya Tyagi <shriya243@gmail.com>
2022-04-21 01:44:01 +0400

    Added Resolver- Avalanche

--- contracts/resolver/avalanche/variables.sol
+++ contracts/resolver/avalanche/variables.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import {InstaFlashloanAggregatorInterface, IAaveProtocolDataProvider} from "./interfaces.sol";
+import "./interfaces.sol";
 
 contract Variables {
     address public constant aaveLendingAddr =
@@ -10,6 +10,8 @@ contract Variables {
         0x65285E9dfab318f57051ab2b139ccCf232945451;
     IAaveProtocolDataProvider public constant aaveProtocolDataProvider =
         IAaveProtocolDataProvider(aaveProtocolDataProviderAddr);
+    IAaveV3DataProvider public constant aaveV3DataProvider =
+        IAaveV3DataProvider(0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654);
 
     address private flashloanAggregatorAddr =
         0x2b65731A085B55DBe6c7DcC8D717Ac36c00F6d19;
