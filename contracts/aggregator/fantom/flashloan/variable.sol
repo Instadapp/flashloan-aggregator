f88e19fb58fab68211d653987e6ed4100cdea256
Shriya Tyagi <47134275+shriyatyagii@users.noreply.github.com>
2022-06-28 18:34:50 +0530

    Update contracts/aggregator/fantom/flashloan/variable.sol
    Co-authored-by: Thrilok kumar <thrilok2000@gmail.com>

--- contracts/aggregator/fantom/flashloan/variable.sol
+++ contracts/aggregator/fantom/flashloan/variable.sol
@@ -5,9 +5,7 @@ import "../../../common/variables.sol";
 contract ConstantVariables {
     address public constant treasuryAddr =
         0x6C4061A00F8739d528b185CC683B6400E0cd396a;
-    address private constant instaListAddr =
-        0x10e166c3FAF887D8a61dE6c25039231eE694E926;
-    ListInterface public constant instaList = ListInterface(instaListAddr);
+    ListInterface public constant instaList = ListInterface(0x10e166c3FAF887D8a61dE6c25039231eE694E926);
 }
 
 contract Variables is ConstantVariables {
