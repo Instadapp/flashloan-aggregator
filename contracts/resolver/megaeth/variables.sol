bf5060d0532ce30368de392390c4c95f1b9d2ffb
aazhou1 <andrew@termfinance.io>
2026-07-29 13:38:31 -0700

    megaeth support

---
+++ contracts/resolver/megaeth/variables.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./interfaces.sol";
+
+contract Variables {
+    IAaveV3DataProvider public constant aaveV3DataProvider =
+        IAaveV3DataProvider(0x9588b453A4EE24a420830CB3302195cA7aA3b403);
+
+    address private flashloanAggregatorAddr =
+        0x9ED23816Ed080427B1d2140Eb6E5BC11D2DAe32f;//TODO: Update this once flashloan aggregator deployed
+    
+    InstaFlashloanAggregatorInterface internal flashloanAggregator =
+        InstaFlashloanAggregatorInterface(flashloanAggregatorAddr);
+    
+    address public constant wethAddr = 0x4200000000000000000000000000000000000006;
+    address public constant usdt0Addr = 0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb;
+   
+    address public constant uniswapFactoryAddr = 0x3a5F0CD7d62452b7f899B2A5758BFa57be0dE478; 
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+
+    struct PoolKey {
+        address token0;
+        address token1;
+        uint24 fee;
+    }
+}
